2d2361bd94710311c76a89d61307351dfdc654b7
willian-kaminski <developer.williansantos@gmail.com>
2021-05-16 19:20:42 -0300

    adding validation profile.

--- src/main/java/com/kaminski/book/service/implementation/ProfileServiceImpl.java
+++ src/main/java/com/kaminski/book/service/implementation/ProfileServiceImpl.java
@@ -3,6 +3,7 @@ package com.kaminski.book.service.implementation;
 import com.kaminski.book.entity.Profile;
 import com.kaminski.book.repository.ProfileRepository;
 import com.kaminski.book.service.ProfileService;
+import com.kaminski.book.utils.Validation;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,11 @@ import java.util.Optional;
 public class ProfileServiceImpl implements ProfileService {
 
     private ProfileRepository profileRepository;
+    private Validation validation;
 
     @Override
     public Profile getProfile(Integer id) {
+        validation.verifyIfProfileExists(id);
         Optional<Profile> profile = profileRepository.findById(id);
         if(profile.isPresent())
             return profile.get();
@@ -24,6 +27,7 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Override
     public Profile getProfileByName(String name) {
+        validation.verifyIfProfileExists(name);
         Optional<Profile> profile = profileRepository.findByName(name);
         if(profile.isPresent())
             return profile.get();
--- src/main/java/com/kaminski/book/utils/Validation.java
+++ src/main/java/com/kaminski/book/utils/Validation.java
@@ -2,11 +2,13 @@ package com.kaminski.book.utils;
 
 import com.kaminski.book.entity.Book;
 import com.kaminski.book.entity.Category;
+import com.kaminski.book.entity.Profile;
 import com.kaminski.book.exception.BookAlreadyRentedException;
 import com.kaminski.book.exception.ResourceNotFoundException;
 import com.kaminski.book.model.BookStatus;
 import com.kaminski.book.repository.BookRepository;
 import com.kaminski.book.repository.CategoryRepository;
+import com.kaminski.book.repository.ProfileRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +20,24 @@ public class Validation {
 
     private BookRepository bookRepository;
     private CategoryRepository categoryRepository;
+    private ProfileRepository profileRepository;
 
     public void verifyIfBookExists(Integer id){
         Optional<Book> book = bookRepository.findById(id);
         if(book.isEmpty())
             throw new ResourceNotFoundException(String.format("Book not found for id %s.", id));
+    }
+
+    public void verifyIfProfileExists(Integer id){
+        Optional<Profile> profile = profileRepository.findById(id);
+        if(profile.isEmpty())
+            throw new ResourceNotFoundException(String.format("Profile not found for id %s.", id));
+    }
+
+    public void verifyIfProfileExists(String name){
+        Optional<Profile> profile = profileRepository.findByName(name);
+        if(profile.isEmpty())
+            throw new ResourceNotFoundException(String.format("Profile not found for name %s.", name));
     }
 
     public void verifyIfBookIsRented(Integer id){
